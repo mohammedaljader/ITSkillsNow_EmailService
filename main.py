9556812aa9a83a3375a7f3a5786fe88a788dc68a
Mohammed Aljader <461166@student.fontys.nl>
2023-05-27 14:58:51 +0200

    try with different way

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
-import asyncio
-
 from fastapi import FastAPI
-from aio_pika import connect, IncomingMessage
 
 app = FastAPI()
-
-
-async def process_message(message: IncomingMessage):
-    async with message.process():
-        # Process the received message here
-        print("Received message:", message.body.decode())
-
-
-async def start_receiver():
-    try:
-        connection = await connect("amqp://guest:guest@localhost/")
-        channel = await connection.channel()
-        queue = await channel.get_queue("course_user.queue")
-        await queue.consume(process_message)
-
-    except Exception as e:
-        print("An error occurred:", str(e))
-
-
-@app.on_event("startup")
-async def startup_event():
-    # Start the receiver
-    asyncio.create_task(start_receiver())
 
 
 @app.get("/")
