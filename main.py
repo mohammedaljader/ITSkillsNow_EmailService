01386060f7f101cce3eb564dfa19978a85950ba0
Mohammed Aljader <461166@student.fontys.nl>
2023-06-04 23:47:42 +0200

    some progress but still need work

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI, BackgroundTasks
+from send_email import send_email_background, send_email_async
 
-app = FastAPI()
+app = FastAPI(title='How to Send Email')
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/')
+def index():
+    return 'Hello World'
+
+
+@app.get('/send-email/asynchronous')
+async def send_email_asynchronous():
+    await send_email_async('Hello World', 'someemail@gmail.com', {
+        'title': 'Hello World',
+        'name': 'John Doe'
+    })
+    return 'Success'
+
+
+@app.get('/send-email/backgroundtasks')
+def send_email_backgroundTasks(background_tasks: BackgroundTasks):
+    send_email_background(background_tasks, 'Hello World', 'someemail@gmail.com', {
+        'title': 'Hello World',
+        'name': 'John Doe'
+    })
+    return 'Success'
 
 
 if __name__ == '__main__':
