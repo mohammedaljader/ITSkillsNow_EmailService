03f3d179826660a75bb989ef499bb0e59d065e72
Mohammed Aljader <461166@student.fontys.nl>
2023-06-05 21:38:31 +0200

    updated

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import uvicorn
 from fastapi import FastAPI
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import smtplib
+from jinja2 import Environment, FileSystemLoader
 
 app = FastAPI()
 
 
 @app.post("/send-email/")
 def send_email(email: str, subject: str, message: str):
-    # SMTP server details
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
     sender_email = "s6.mohammedaljader@gmail.com"
     sender_password = "oapplcpiborcrxfd"
+
+    # Initialize Jinja2 environment
+    env = Environment(loader=FileSystemLoader("templates"))
+
+    # Load and render HTML template
+    template = env.get_template("email.html")
+    html_content = template.render(title=subject, name="Mohammed", code="4832", message=message)
+
+    # Create a multipart message container
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = subject
+    msg["From"] = sender_email
+    msg["To"] = email
+
+    # Attach the HTML content to the email
+    msg.attach(MIMEText(html_content, "html"))
 
     # Connect to the SMTP server
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls()
     server.login(sender_email, sender_password)
 
-    # Compose the email
-    email_content = f"Subject: {subject}\n\n{message}"
-
     # Send the email
-    server.sendmail(sender_email, email, email_content)
+    server.sendmail(sender_email, email, msg.as_string())
 
     # Close the connection
     server.quit()
